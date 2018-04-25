d7d001fac3169b0a035b9690ddf5c4a384f71a85
Gustavo <guatavo.lima@icloud.com>
2018-04-25 15:03:44 -0300

    calc

--- 2-Calculadora/calculadora/src/app/app.module.ts
+++ 2-Calculadora/calculadora/src/app/app.module.ts
@@ -1,16 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
 
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule} from '@angular/core';
+
+
+import { MaterializeModule } from 'angular2-materialize';
 
 import { AppComponent } from './app.component';
 
+
+import { CalculadoraModule } from './calculadora';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CalculadoraModule,
+    MaterializeModule
+    
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
